e293fb8ef4bab4eff2a590ebb44214608a107ed6
Daniel Lomelino <dlomelino@pm.me>
2021-05-26 12:46:22 -0400

    Second iteration of Poker

--- poker/poker.py
+++ poker/poker.py
@@ -10,6 +10,18 @@ import random
 # num-of-players
 # copy ?
 
+#player and table tokens
+player_tokens = 1000
+table_tokens = 1000
+# win conditions in order of importance
+# s_flush: five cards of the same suit in sequence
+# four_kind: Four cards of the same rank (e.g. A♣, A♠, A♥, A◆)
+# full_house: three cards of one rank and two cards of another rank
+# flush: Five cards of the same suit.
+# straight: Five cards of mixed suits in sequence
+# three_kind: Three cards of the same rank plus two unequal cards
+# two_pairs: two cards of equal rank
+# 
 
 def build_deck():
     """build an ordered deck using list comprehensions (thanks"""
@@ -23,51 +35,86 @@ def build_deck():
     return new_deck
 
 
-def pokerdealer(num):
+def pokerdealer():
     """deal a random hand of num cards"""
+    global player_tokens
+    global table_tokens
     random_deck = build_deck()
     random.shuffle(random_deck)
     player_hand = []
     # deal the hands"
     computer_hand = []
-    while len(player_hand) < num:
+    while len(computer_hand) < 5:
         player_hand.append(random_deck.pop())
-
-    while len(computer_hand) < num:
-        computer_hand.append("**")
-
-    for card in computer_hand:
-        # hide_it
-        pass
-    loop = 0
-    while loop < 3:
-        print(
-            f"here is your hand of {num} cards: {random_deck[-num:]} \n computerhand: {computer_hand}")
-        mulligan_choices = input(
-            "what are the indices of the cards you want to mulligan in a csv or 'n':")
-        if mulligan_choices == "n":
-            loop = 3
-            print("ok you can keep those cards")
-            continue
-        for choice in mulligan_choices.split(","):
-            player_hand[int(choice)] = random_deck.pop()
-        loop += 1
-    # fix the computer hand for printing
-    computer_hand.clear()
-    while len(computer_hand) < num:
         computer_hand.append(random_deck.pop())
-    print(
-        f"here is your hand of {num} cards: {random_deck[-num:]} \n computerhand: {computer_hand}")
+
+    # while len(computer_hand) < num:
+        # computer_hand.append(random_deck.pop())
+        # not removing card from the deck
+        # computer_hand.append("**")
+        # suggestion: actually append card here, display ** in print
+
+
+# unneccessary for loop? resolved on line 50
+    # for card in computer_hand:
+    #     card.append("**")
+    loop = 0
+    table_pot = 10
+    auntie = 2
+    place_bets = input("starting bet is 10 tokens.  Do you wish to proceed?")
+    if place_bets.lower() == "yes" or place_bets.lower() == "y":
+        player_tokens -= 10
+        table_pot += 10
+        while loop < 3:
+            print(f"Current pot: {table_pot}")
+            print(
+                f"""here is your hand of {5} cards: {player_hand} \n computerhand: {[ "**" for _ in computer_hand]}""")
+            mulligan_choices = input(
+                "what are the indices of the cards you want to mulligan in a csv or 'n': ")
+            if mulligan_choices == "n":
+                print("ok you can keep those cards")
+                continue
+            else:
+                for choice in mulligan_choices.split(","):
+                    player_hand[int(choice)] = random_deck.pop()
+                print(player_hand)
+                fold_or_stay = input("Fold or stay?: ")
+                if fold_or_stay.lower() == "fold" or fold_or_stay[0].lower() == "f":
+                    break
+                raised_or_call = input("Are you going to raise or call? ")
+                if raised_or_call.lower() == "r" or raised_or_call.lower() == "raise":
+                    auntie = input("Up your ante: ")
+                    while auntie:
+                        try:
+                            while int(auntie) > player_tokens:
+                                auntie = input("Too big for your britches.  Bet in your range: ")
+                            player_tokens -= int(auntie)
+                            table_pot += int(auntie)
+                            break
+                        except (ValueError):
+                            auntie = input("That's not a number.  What's your ante?: ")
+                else:
+                    player_tokens -= auntie
+                    table_pot += auntie
+            loop += 1
+    # fix the computer hand for printing
+    # computer_hand.clear()
+    # may need to be sooner.
+    # while len(computer_hand) < num:
+    #     computer_hand.append(random_deck.pop())
+        print(
+            f"here is your hand of {5} cards: {player_hand} \n computerhand: {computer_hand}")
+    print("Come back when you have the tokens")
 
 
 def main(args):
     """Main program code."""
-    if len(args) != 1:
-        print('usage: python pokerhands.py num_of_cards')
+    if len(args) > 0:
+        print('usage: python pokerhands.py')
         sys.exit(1)
 
-    num = sys.argv[1]
-    pokerdealer(int(num))
+    # num = sys.argv[1]
+    pokerdealer()
 
 
 if __name__ == '__main__':
