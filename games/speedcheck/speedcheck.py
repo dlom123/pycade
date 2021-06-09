ef9fee14c90e0bc87b53ff9977253926111a5fa9
Marcus Chiriboga <marcuschiriboga@gmail.com>
2021-06-09 13:52:55 -0400

    before merging into main.py

--- games/speedcheck/speedcheck.py
+++ games/speedcheck/speedcheck.py
@@ -15,7 +15,6 @@ calculate how long it took
 import time
 from datetime import datetime
 import random
-
 exit_flag = None
 game_tokens = None
 game_username = None
@@ -23,38 +22,59 @@ game_username = None
 
 def init():
     global exit_flag, game_tokens, game_username, time
-    game_tokens = 0
-    exix_flag = False
+    exit_flag = False
+
+
+def input_continue_playing():
+    """
+    Make sure the player has enough tokens to continue playing, then ask them
+    if they would like to continue playing, or walk away.
+    """
+    global exit_flag
+    if game_tokens <= 0:
+        exit_flag = True
+        print("Thank you for playing, youre broke now!")
+        return
+
+    continue_playing = input("Would you like to keep playing? (y/n)")
+
+    if continue_playing == "n":
+        exit_flag = True
+        return
 
 
 def play(username, tokens):
     init()
+    global game_tokens
     game_tokens = tokens
     game_username = username
 
+    bet = 0
     while not exit_flag:
-        print('countdown')
+        bet = int(input("How many tokens will you bet?(int)"))
+        # if bet <= game_tokens and bet >= 0:
+        #     pass
+        # helpers.update_tokens(game_username, game_tokens-bet)
+        print('countdown')  # needs animation
         time.sleep(random.randint(1, 3))
-        time1 = datetime.now()
-        input("go")
-        time2 = datetime.now()
+        time1 = datetime.now()  # check the time
+        input("go\n====\n")  # take input
+        time2 = datetime.now()  # check the time
         print(f"time1:{time1} \ntime2: {time2}")
         reaction_speed = time2-time1
         print(str(reaction_speed)[-9:])
         if float(str(reaction_speed)[-9:]) < 2:
-            print("wow you are so fast, you need some tokens")
-            # play again
+            print(f"wow {game_username} are so fast, you earned {bet} tokens")
+            input_continue_playing()
             # reward tokens
+            # update_token_amount = game_tokens + (bet*(2-reaction_speed))
+            # helpers.update_tokens(game_username, updated_tokens_amount)
+            # helper funtion updates 'database'
         else:
             print("you're too slow")
+            input_continue_playing()
             # play again?
             # don't reward tokens.
-        # ready set (delay) random time (1 - 3 seconds)
-        # marking the time it posts
-        # wait for input.
-        # calculate how long it took
-
-    pass
 
 
 play("marcus", 100)
