1064b163212ea4aec7020b046329457787acd095
perrymw <perry357@outlook.com>
2021-06-23 14:00:32 -0400

    Try except block is not being applied and enemy placement function utilizes memoization.

--- games/battleship/battleship.py
+++ games/battleship/battleship.py
@@ -1,8 +1,8 @@
 import random
 
 still_playing = None
-game_tokens = None
-game_username = None
+game_tokens = 20
+game_username = "Cap'n"
 
 
 def init():
@@ -13,7 +13,8 @@ def init():
 rows = "ABCDEFGHIJ"
 column = list(range(1, 11))
 game_board = {row: [f'{row}{number}' for number in column] for row in rows}
-
+tracker_board = {row: [f'{row}{number}' for number in column] for row in rows}
+row = [True, False]
 ship_dict = {"C": 5, "Bb": 4, "Gb": 3, "S": 3}
 ship_name_dict = {"C": ["Carrier", 2],
                   "Bb": ["Battleship", 3],
@@ -22,35 +23,40 @@ ship_name_dict = {"C": ["Carrier", 2],
 ships = [2, 3, 3, 4, 5]
 
 
-def enemy_placement():
-    ship_locale = {}
-    row = [True, False]
-    tracker_board = {row: [f'{row}{number}' for number in column] for row in rows} # noqa
+def enemy_placement(ship_locale={}):
     for ship in ship_dict:
         ship_pos = []
-        orientation = random.choice(row)
-        rand_row = random.choice(list(tracker_board.keys()))
-        start = random.choice(tracker_board[rand_row])
-        if orientation and int(start[1:]) > ship_dict[ship]+1:
-            start = start[0] + str(10-ship_dict[ship]+1)
-        elif not orientation and rows.find(rand_row) + 1 > ship_dict[ship]:
-            start = f"""{rows[len(tracker_board[rand_row])-ship_dict[ship]]}
-            {start[1:]}"""
-        ship_pos.append(start)
-        for _ in range(ship_dict[ship]-1):
-            if orientation:
-                new_col = int(start[1:])
-                new_col_pos = tracker_board[rand_row][new_col]
-                ship_pos.append(new_col_pos)
-                start = new_col_pos
-            else:
-                new_row = rows[rows.find(start[0])+1]
-                next_pos = tracker_board[new_row][int(start[1:])-1]
-                ship_pos.append(next_pos)
-                start = f"{next_pos[0]}{start[1:]}"
-        for pos in ship_pos:
-            tracker_board[pos[0]].pop(tracker_board[pos[0]].index(pos))
-        ship_locale[ship] = ship_pos
+        if ship not in ship_locale:
+            orientation = random.choice(row)
+            rand_row = random.choice(list(tracker_board.keys()))
+            start = random.choice(tracker_board[rand_row])
+            if start == "X":
+                return enemy_placement(ship_locale=ship_locale)
+            elif orientation and int(start[1:]) > ship_dict[ship]+1:
+                start = start[0] + str(10-ship_dict[ship]+1)
+            elif not orientation and rows.find(rand_row) + 1 > ship_dict[ship]:
+                start = f"""{rows[len(tracker_board[rand_row])-ship_dict[ship]]}{start[1:]}"""
+            ship_pos.append(start)
+            for _ in range(ship_dict[ship]-1):
+                if orientation:
+                    new_col = int(start[1:])
+                    new_col_pos = tracker_board[rand_row][new_col]
+                    if new_col_pos == "X":
+                        return enemy_placement(ship_locale=ship_locale)
+                    ship_pos.append(new_col_pos)
+                    start = new_col_pos
+                else:
+                    new_row = rows[rows.find(start[0])+1]
+                    next_pos = tracker_board[new_row][int(start[1:])-1]
+                    if next_pos == "X":
+                        return enemy_placement(ship_locale=ship_locale)
+                    ship_pos.append(next_pos)
+                    start = f"{next_pos[0]}{start[1:]}"
+            for pos in ship_pos:
+                track_row = tracker_board[pos[0]]
+                index = track_row.index(pos)
+                track_row[index] = "X"
+            ship_locale[ship] = ship_pos
     return ship_locale
 
 
@@ -63,14 +69,11 @@ def play(username, tokens):
     game_username = username
     sunken_ships = []
     guess_list = []
-
+    # try:
+    position = enemy_placement()
+    # except Exception:
+    #     play(username, tokens)
     print(f"Battleship\tTokens: {game_tokens}\n")
-
-    try:
-        position = enemy_placement()
-    except Exception:
-        play(username, tokens)
-    print(f"You have {game_tokens} tokens")
     print("Welcome to Battleship! Run out of guesses and good-bye fleet.")
     decision = input("Would you like to play?[y/n] ")
     if decision.lower() == "y":
@@ -125,4 +128,4 @@ def play(username, tokens):
 
 
 if __name__ == '__main__':
-    play()
+    play(game_username, game_tokens)
